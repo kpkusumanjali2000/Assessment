a851a870ef3e1d0e96312a3c7aa16e8af472d559
kpkusumanjali2000 <kusumanjalikp1995@gmail.com>
2023-12-07 13:12:34 +0530

    feat: :chart_with_upwards_trend: added product page

--- flipcart-clone/app/page.js
+++ flipcart-clone/app/page.js
@@ -1,6 +1,16 @@
 import Image from 'next/image';
 import Layout from '@/components/Layout';
+import ProductItem from '../components/ProductItem';
+import data from '../utils/Data';
 
 export default function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
